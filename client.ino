c849ae4edd089a451c6111c6b6ac301a5bd6d060
Godó Bence <52776215+godesz@users.noreply.github.com>
2020-05-14 22:11:56 +0200

    base commit, 2 holes, 10+5 points
    8 channel Amux, working with 2 holes

---
+++ client.ino
@@ -0,0 +1,163 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+// Constants
+const char* ssid = "Hooper_v1";
+const char* password = "qwert123";
+const char *host = "192.168.4.1";
+
+HTTPClient http;    //Declare object of class HTTPClient
+String  station, getData;
+String Link10 = "http://192.168.4.1/station10";
+String Link5 = "http://192.168.4.1/station5";
+int httpGETReq;
+String GOT_str;
+
+int LDR_value, LDR_base_value = 0, LDR_threshold = 500;
+int LDR_value2, LDR_base_value2 = 0, LDR_threshold2 = 500;
+int selectPIN1 = D4, selectPIN2 = D3, selectPIN3 = D2;
+
+int max_score = 0, score = 0;
+
+int last_TT = millis(), triggered = 0;
+
+//=======================================================================
+//                    Selecting PIN of MUX
+//=======================================================================
+int get_mux(int pin)
+{
+  digitalWrite (selectPIN1, (pin & 1) ? HIGH : LOW);  // low-order bit
+  digitalWrite (selectPIN2, (pin & 2) ? HIGH : LOW);
+  digitalWrite (selectPIN3, (pin & 4) ? HIGH : LOW);  // high-order bit
+  return analogRead(A0);
+}
+
+//=======================================================================
+//                    Adjusting LDR base value
+//=======================================================================
+void adjust_LDR()
+{ 
+  Serial.println("Calibrating LDR_1...");
+  int local_A0, i, N = 128;
+  LDR_base_value = 0;
+  for(i=0; i<N;i++)
+  {
+    local_A0 = get_mux(1);
+    LDR_base_value += local_A0;
+    delay(1);
+  }
+  LDR_base_value = LDR_base_value / N ;
+  Serial.print("LDR calibrated for base value: ");
+  Serial.println(LDR_base_value);
+  LDR_threshold = LDR_base_value * 0.9;
+
+  Serial.println("Calibrating LDR_2...");
+  local_A0 = 0;
+  LDR_base_value2 = 0;
+  for(i=0; i<N;i++)
+  {
+    local_A0 = get_mux(3);
+    LDR_base_value2 += local_A0;
+    delay(1);
+  }
+  LDR_base_value2 = LDR_base_value2 / N ;
+  Serial.print("LDR_2 calibrated for base value: ");
+  Serial.println(LDR_base_value2);
+  LDR_threshold2 = LDR_base_value2 * 0.9;
+  
+}
+//=======================================================================
+//                    Make GET
+//=======================================================================
+void make_get(int value)
+{
+  if (value == 10)  http.begin(Link10);     //Specify request destination
+  if (value == 5)   http.begin(Link5);
+  else http.begin(Link10);
+  
+  httpGETReq = http.GET();            //Send the request
+  GOT_str = http.getString();    //Get the response payload
+
+  Serial.println(httpGETReq);   //Print HTTP return code, 200 normally
+  Serial.println(GOT_str);    //Print request response payload
+
+  http.end();  //Close connection
+}
+
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+   
+  pinMode(selectPIN1, OUTPUT);
+  pinMode(selectPIN2, OUTPUT);
+  pinMode(selectPIN3, OUTPUT);
+  delay(3000);
+  adjust_LDR();
+  delay(3000);
+  /*
+  WiFi.mode(WIFI_OFF);        
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     
+  Serial.println("");
+
+  Serial.print("Connecting");  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+*/
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+
+
+
+  LDR_value = get_mux(1);
+  LDR_value2 = get_mux(3);
+  if ( (LDR_value < LDR_threshold) && (millis() - last_TT > 1000))
+  {
+    last_TT = millis();
+    triggered = 255;
+    make_get(10);  // sending a GET to the server to make a score
+  }
+
+
+  
+  if ( (LDR_value2 < LDR_threshold2) && (millis() - last_TT > 1000))
+  {
+    last_TT = millis();
+    triggered = 255;
+    make_get(5);  // sending a GET to the server to make a score
+  }
+  
+  //if ( (LDR_value > LDR_threshold) && (millis() - last_TT > 1000))
+  //{    triggered = 0;  }
+  
+  Serial.print(0);  Serial.print("\t");
+  Serial.print(1023);  Serial.print("\t");
+  Serial.print(triggered); Serial.print("\t");
+  Serial.print(LDR_value); Serial.print("\t");
+  Serial.print(LDR_value2); Serial.print("\t");
+  Serial.print(LDR_threshold); Serial.print("\t");
+  Serial.print(LDR_threshold2); Serial.print("\n");
+  triggered = 0;
+  //delay(5000);  //GET Data at every 5 seconds
+}
+//=======================================================================
---
+++ hoop_server.ino
@@ -0,0 +1,165 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+
+const char* ssid     = "Hooper_v1";
+const char* password = "qwert123";
+
+int player_score = 0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;  
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>Hoop v1.0 Server</h2>
+  <p>
+    <span class="dht-labels">Point</span> 
+    <span id="score">%SCORE%</span>
+  </p>
+  <p>
+    <span class="dht-labels">Record</span>
+    <span id="record">%RECORD%</span>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("score").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/score", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("record").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/record", true);
+  xhttp.send();
+}, 10000 ) ;
+
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "SCORE"){
+    return String(player_score);
+  }
+  else if(var == "RECORD"){
+    return String(120);
+  }
+  else if(var == "STATION10"){
+    return String(player_score);
+  }
+  else if(var == "STATION5"){
+    return String(player_score);
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/score", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(player_score).c_str());
+  });
+  server.on("/record", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(120).c_str());
+  });
+  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String("RESET").c_str());
+    player_score = 0;
+  });
+  server.on("/station10", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.print("Got GET ->");
+    Serial.println(player_score);
+    player_score += 10;
+    request->send_P(200, "text/plain", String(player_score).c_str());
+  });
+  server.on("/station5", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.print("Got GET ->");
+    Serial.println(player_score);
+    player_score += 5;
+    request->send_P(200, "text/plain", String(player_score).c_str());
+  });
+  // Start server
+  server.begin();
+}
+ 
+void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+
+    
+      //Serial.println();
+    }
+}
+
